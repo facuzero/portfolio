0cb9172c614929908812bcfa9f38b18f4888b1e1
facuzero <facuzero_95@hotmail.com>
2025-09-30 21:16:14 -0300

    more fixes

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,9 +1,11 @@
 import { ui, defaultLang, showDefaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
+  // "/portfolio" en GitHub, "" en localhost
   const base = import.meta.env.BASE_URL.replace(/^\/|\/$/g, '');
   const parts = url.pathname.split('/').filter(Boolean);
 
+  // Si hay base, idioma viene después
   const langIndex = base ? 1 : 0;
 
   const lang = parts[langIndex];
@@ -11,16 +13,13 @@ export function getLangFromUrl(url: URL) {
 
   return defaultLang;
 }
-
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
   };
 }
-
 export function useTranslatedPath(lang: keyof typeof ui) {
-  const base = import.meta.env.BASE_URL.replace(/\/$/, '');
   return function translatePath(path: string, l: string = lang) {
-    return !showDefaultLang && l === defaultLang ? `${base}${path}` : `${base}/${l}${path}`;
+    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`; // esto agregará /es o /en
   };
 }
