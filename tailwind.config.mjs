8243f675a77affd8bcf208b34d1e5bda182c43cb
facuzero <facuzero_95@hotmail.com>
2025-10-07 15:45:50 -0300

    fix theme

--- tailwind.config.mjs
+++
@@ -1,7 +0,0 @@
-// tailwind.config.mjs
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: ['selector', '[data-theme="dark"]'],
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,mdx,md}'],
-  plugins: [],
-};
